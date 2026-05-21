9dc29e73a94e078b8948eefb1b2d2682640e61ec
Alperen Ozalp <alperennozalp@gmail.com>
2026-05-21 22:20:30 +0300

    test: add endpoint tests

---
+++ tests/test_endpoints.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_ping_endpoint():
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "pong"}
+
+
+def test_healthz_endpoint():
+    response = client.get("/healthz")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
